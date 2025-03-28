9589be416a32a4df111473d8979fbd0a5f19aabc
krzysztofmotas <127015975+krzysztofmotas@users.noreply.github.com>
2025-03-28 17:21:48 +0100

    inicjalizacja backendu w spring boot

---
+++ backend/src/main/java/com/hoteltaskmanager/HotelTaskManagerApplication.java
@@ -0,0 +1,13 @@
+package com.hoteltaskmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelTaskManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelTaskManagerApplication.class, args);
+	}
+
+}
