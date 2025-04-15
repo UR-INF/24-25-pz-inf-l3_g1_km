a9c7e680973b6a8cfe4b4ed652d318db21de2210
Jakub Orczyk <j.orczyk.it@gmail.com>
2025-04-15 16:38:17 +0200

    Modyfikowanie rezerwacji

--- frontend/src/views/Receptionist/ReservationDetails.tsx
+++ frontend/src/views/Receptionist/ReservationDetails.tsx
@@ -1,12 +1,17 @@
+import React, { useState } from "react";
 import ModifyReservation from "../../components/ModifyReservation";
+import { useLocation } from "react-router";
 
 const ReservationsDetails = () => {
+  const location = useLocation();
+  const reservationId = location.state?.reservationId ?? 1;
+  
   return (
     <div className="page-wrapper">
       <div className="page-body">
         <div className="container-xl">
           <div className="card">
-            <ModifyReservation />
+            <ModifyReservation reservationId={reservationId}/>
           </div>
         </div>
       </div>
