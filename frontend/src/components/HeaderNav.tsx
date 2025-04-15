0738475428ce90fe19270a3359b1a26085ac280e
krzysztofmotas <127015975+krzysztofmotas@users.noreply.github.com>
2025-04-15 11:45:41 +0200

    refaktoryzacja i poprawki błędów w HeaderNav

--- frontend/src/components/HeaderNav.tsx
+++ frontend/src/components/HeaderNav.tsx
@@ -2,6 +2,7 @@ import { useUser } from "../contexts/user";
 import { useAuth } from "../contexts/auth";
 import { useNavigate } from "react-router";
 import { getRoleNameInPolish } from "../utils/roleUtils";
+import { Link } from "react-router";
 
 const HeaderNav = () => {
   const { userFirstName, userLastName, userRoleName } = useUser();
@@ -13,121 +14,90 @@ const HeaderNav = () => {
     navigate("/login", { replace: true });
   };
 
-  const handleClickDashboard = () => {
-    navigate("/", { replace: true });
-  };
-
-  const handleClickReservation = () => {
-    navigate("/RecepcionistDashboard/Reservations", { replace: true });
-  };
-
-  const handleClickCleaningOrders = () => {
-    navigate("/RecepcionistDashboard/CleaningOrders", { replace: true });
-  };
-
-  const handleClickRepairsOrders = () => {
-    navigate("/RecepcionistDashboard/RepairsOrders", { replace: true });
-  };
-
   return (
-    <header className="navbar-expand-md">
-      <div className="collapse navbar-collapse" id="navbar-menu">
-        <div className="navbar">
-          <div className="container-xl">
-            <div className="row flex-fill align-items-center">
-              <div className="col">
-                <ul className="navbar-nav">
-                  <li className="nav-item active">
-                    <a className="nav-link" href="" onClick={handleClickDashboard}>
-                      <span className="nav-link-icon d-md-none d-lg-inline-block">
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          width="24"
-                          height="24"
-                          viewBox="0 0 24 24"
-                          fill="none"
-                          stroke="currentColor"
-                          stroke-width="2"
-                          stroke-linecap="round"
-                          stroke-linejoin="round"
-                          className="icon icon-1"
-                        >
-                          <path d="M5 12l-2 0l9 -9l9 9l-2 0" />
-                          <path d="M5 12v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-7" />
-                          <path d="M9 21v-6a2 2 0 0 1 2 -2h2a2 2 0 0 1 2 2v6" />
-                        </svg>
-                      </span>
-                      <span className="nav-link-title"> Dashboard </span>
-                    </a>
-                  </li>
-                  <li className="nav-item">
-                    <a className="nav-link" href="" onClick={handleClickReservation}>
-                      <span className="nav-link-title"> Rezerwacje </span>
-                    </a>
-                  </li>
-                  <li className="nav-item dropdown">
-                    <a
-                      className="nav-link dropdown-toggle"
-                      href="#navbar-extra"
-                      data-bs-toggle="dropdown"
-                      data-bs-auto-close="outside"
-                      role="button"
-                      aria-expanded="false"
-                    >
-                      <span className="nav-link-title"> Zgłoszenia </span>
-                    </a>
-                    <div className="dropdown-menu">
-                      <div className="dropdown-menu-columns">
-                        <div className="dropdown-menu-column">
-                          <a className="dropdown-item" href="" onClick={handleClickCleaningOrders}>
-                            {" "}
-                            Zgłoszenia sprzątania{" "}
-                          </a>
-                          <a className="dropdown-item" href="" onClick={handleClickRepairsOrders}>
-                            {" "}
-                            Zgłoszenia naprawy{" "}
-                          </a>
-                        </div>
-                      </div>
-                    </div>
-                  </li>
-                  <li></li>
-                </ul>
-              </div>
-            </div>
-            <div className="nav-item dropdown">
-              <a
-                href="#"
-                className="nav-link d-flex lh-1 p-0 px-2"
-                data-bs-toggle="dropdown"
-                aria-label="Open user menu"
-              >
-                <span
-                  className="avatar avatar-sm"
-                  style={{
-                    backgroundImage:
-                      "url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRpAklFXnBaXsEsy_Y08157gUwnacNh7gQgLQ&s')",
-                  }}
-                ></span>
+    <header className="navbar navbar-expand">
+      <div className="container-xl">
+        <div className="row flex-fill align-items-center">
+          <div className="col">
+            <ul className="navbar-nav">
+              <li className="nav-item active">
+                <Link className="nav-link" to="/" replace>
+                  <span className="nav-link-icon">
+                    <i className="ti ti-home fs-2"></i>
+                  </span>
+                  <span className="nav-link-title">Dashboard</span>
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link className="nav-link" to="/RecepcionistDashboard/Reservations" replace>
+                  <span className="nav-link-icon">
+                    <i className="ti ti-calendar-check fs-2"></i>
+                  </span>
+                  <span className="nav-link-title">Rezerwacje</span>
+                </Link>
+              </li>
+              <li className="nav-item dropdown">
+                <a
+                  className="nav-link dropdown-toggle"
+                  href="#navbar-extra"
+                  data-bs-toggle="dropdown"
+                  data-bs-auto-close="outside"
+                  role="button"
+                  aria-expanded="false"
+                >
+                  <span className="nav-link-icon">
+                    <i className="ti ti-report fs-2"></i>
+                  </span>
 
-                <div className="ps-2">
-                  <div>
-                    {userFirstName} {userLastName}
-                  </div>
-                  <div className="mt-1 small text-secondary">
-                    {getRoleNameInPolish(userRoleName)}
-                  </div>
+                  <span className="nav-link-title">Zgłoszenia</span>
+                </a>
+                <div className="dropdown-menu">
+                  <Link
+                    className="dropdown-item"
+                    to="/RecepcionistDashboard/CleaningOrders"
+                    replace
+                  >
+                    Zgłoszenia sprzątania
+                  </Link>
+
+                  <Link className="dropdown-item" to="/RecepcionistDashboard/RepairsOrders" replace>
+                    Zgłoszenia naprawy
+                  </Link>
                 </div>
-              </a>
-              <div className="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
-                <a href="#" className="dropdown-item">
-                  Ustawienia
-                </a>
-                <a href="#" className="dropdown-item text-danger" onClick={handleLogout}>
-                  Wyloguj się
-                </a>
+              </li>
+              <li></li>
+            </ul>
+          </div>
+        </div>
+        <div className="nav-item dropdown">
+          <a
+            href="#"
+            className="nav-link d-flex lh-1 p-0 px-2"
+            data-bs-toggle="dropdown"
+            aria-label="Open user menu"
+          >
+            <span
+              className="avatar avatar-sm"
+              style={{
+                backgroundImage:
+                  "url('https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRpAklFXnBaXsEsy_Y08157gUwnacNh7gQgLQ&s')",
+              }}
+            ></span>
+
+            <div className="ps-2">
+              <div>
+                {userFirstName} {userLastName}
               </div>
+              <div className="mt-1 small text-secondary">{getRoleNameInPolish(userRoleName)}</div>
             </div>
+          </a>
+          <div className="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
+            <a href="#" className="dropdown-item">
+              Ustawienia
+            </a>
+            <a href="#" className="dropdown-item text-danger" onClick={handleLogout}>
+              Wyloguj się
+            </a>
           </div>
         </div>
       </div>
