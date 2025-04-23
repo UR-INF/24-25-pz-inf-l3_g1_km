26a1a4ee792f8f512e25c0ef1454de8e73e86540
utopiasz <s3ndyy3@gmail.com>
2025-04-23 23:23:10 +0200

    hotfix

--- frontend/src/components/CleaningTable.tsx
+++ frontend/src/components/CleaningTable.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router";
 import { api } from "../services/api";
 import { useNotification } from "../contexts/notification";
-import DeleteConfirmationModal from "../views/Receptionist/DeleteConfirmationModal";
+import DeleteConfirmationModal from "./DeleteConfirmationModal";
 
 const CleaningTable = () => {
   const navigate = useNavigate();
@@ -257,6 +257,20 @@ const CleaningTable = () => {
               onClick={() => setCurrentPage((p) => p - 1)}
               disabled={currentPage === 1}
             >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="icon icon-1"
+              >
+                <path d="M15 6l-6 6l6 6"></path>
+              </svg>
               poprzednia
             </button>
           </li>
@@ -274,6 +288,20 @@ const CleaningTable = () => {
               disabled={currentPage === totalPages}
             >
               następna
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                className="icon icon-1"
+              >
+                <path d="M9 6l6 6l-6 6"></path>
+              </svg>
             </button>
           </li>
         </ul>
