857de0db1d43d4353528eb21f343d219f0afc97d
krzysztofmotas <127015975+krzysztofmotas@users.noreply.github.com>
2025-05-31 11:57:06 +0200

    poprawa warningów o przestarzałych funkcjach w JwtConfig.java

--- backend/src/main/java/com/hoteltaskmanager/config/JwtConfig.java
+++ backend/src/main/java/com/hoteltaskmanager/config/JwtConfig.java
@@ -1,9 +1,14 @@
 package com.hoteltaskmanager.config;
 
 import io.jsonwebtoken.*;
+import io.jsonwebtoken.security.Keys;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import java.security.Key;
+
+import java.nio.charset.StandardCharsets;
 import java.util.Date;
 
 /**
@@ -20,26 +25,35 @@ public class JwtConfig {
 
     /**
      * Generuje token JWT dla podanego użytkownika.
+     * 
      * @param username Nazwa użytkownika.
      * @return Wygenerowany token JWT.
      */
     public String generateToken(String username) {
+        Key key = Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
+
         return Jwts.builder()
                 .setSubject(username)
                 .setIssuedAt(new Date())
                 .setExpiration(new Date(System.currentTimeMillis() + expirationTime))
-                .signWith(SignatureAlgorithm.HS256, secretKey)
+                .signWith(key, SignatureAlgorithm.HS256)
                 .compact();
     }
 
     /**
      * Waliduje token JWT i zwraca jego dane.
+     * 
      * @param token Token JWT.
      * @return Informacje zapisane w tokenie.
      */
     public Claims validateToken(String token) {
-        return Jwts.parser()
-                .setSigningKey(secretKey)
+        Key key = Keys.hmacShaKeyFor(secretKey.getBytes(StandardCharsets.UTF_8));
+
+        JwtParser parser = Jwts.parserBuilder()
+                .setSigningKey(key)
+                .build();
+
+        return parser
                 .parseClaimsJws(token)
                 .getBody();
     }
